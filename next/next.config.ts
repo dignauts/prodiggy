6e0b632edea6a0f945ae11c6e0b59f3218ce615e
Norbert Zborecki <norbertzborecki@gmail.com>
2025-05-22 22:03:43 +0200

    feature: github pages

--- next/next.config.ts
+++ next/next.config.ts
@@ -6,6 +6,8 @@ import pkg from './package.json';
 const withNextIntl = createNextIntlPlugin('./i18n/requests.i18n.ts');
 
 const nextConfig: NextConfig = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/prodiggy/' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? '/prodiggy' : undefined,
   env: {
     VERSION: pkg.version
   },
